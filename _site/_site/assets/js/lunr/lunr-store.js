94612fbb4508d9418fe18dd95aa42454c1c1c845
jsw166098 <jsw166098@gmail.com>
2022-11-05 05:03:45 +0900

    about.md

---
+++ _site/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "StyleGAN2-ADA Direction 적용",
+        "excerpt":"Direction 이란     특정 속성(Age, Gender, Smile)에 해당하는 Latent 공간 정보를 의미한다.   Direction 을 통해 한 가지 속성에 대한 변환된 값을 얻을 수 있다.   ","categories": ["Image_to_Image_Translation"],
+        "tags": ["stylegan","code","project"],
+        "url": "http://localhost:4000/image_to_image_translation/stylegan2ada_direction/",
+        "teaser": null
+      },{
+        "title": "StyleGAN2-ADA Train 메뉴얼",
+        "excerpt":"Preparing Datasets Training new networks Generate Image Projecting images to latent space Preparing Datasets StyleGAN2-ADA를 train하기 전에 .tfrecords 형식으로 전처리를 진행한다. jpg, png 와 같은 이미지 파일 형식을 .tfrecords 형식의 확장자로 변환한다. dataset_too.py 내부의 create_from_images를 호출한다. # 실제 실행 코드 python dataset_tool.py create_from_images ~/tfrecords_folder/ ~/image_folder # 설명용 코드 (실행 x)...","categories": ["Image_to_Image_Translation"],
+        "tags": ["stylegan","code","project"],
+        "url": "http://localhost:4000/image_to_image_translation/stylegan2ada_manual/",
+        "teaser": null
+      },{
+        "title": "Useful Markdown Grammar",
+        "excerpt":"Notice   Note: search: false only works to exclude posts when using Lunr as a search provider.   Link   ","categories": ["Markdown"],
+        "tags": ["stylegan","code","project"],
+        "url": "http://localhost:4000/markdown/markdown_g/",
+        "teaser": null
+      },{
+        "title": "SwAV을 이용한 ResNet Fine-tuning",
+        "excerpt":"SwAV 이란 SwAV은 Contrastive Learing 기반의 Self Supervised Learning 방법 중 하나로 label이 없는 데이터에 대하여 훈련을 진행할 수 있다. Contrastive Learning은 입력 샘플 간의 비교를 통해 학습을 수행하는 방법으로, SwAV을 통해서는 unlabeled 이미지에 대한 feature를 학습할 수 있다. SwAV Code train.py 사전 처리 SwAV으로 생성된 pth파일의 경우 multi gpu를...","categories": ["Project"],
+        "tags": ["self_supervised","swav","fine-tuning"],
+        "url": "http://localhost:4000/project/swav_resnet_finetuning/",
+        "teaser": null
+      }]
